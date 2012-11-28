f3cc82580c21cfbe61062eb67dd6afd189fab2a0
matt j. sorenson <matt@sorensonbros.net>
2012-11-28 00:25:18 -0600

    uptick manifest_version to 2 and version to 0.2; made comment.js smaller again

--- comment.js
+++ comment.js
@@ -1,30 +1,17 @@
-(function(d, w, undefined){
+;(function(d){
   var comments = d.getElementsByClassName('comment'), current = -1;
 
-  function move(dir) {
-
-    switch (dir) {
-
-      case 'next':
-        current++;
-        if ( current > comments.length - 1 ) current = 0;
-        break;
-
-      case 'prev':
-        current--;
-        if ( current < 0 ) current = comments.length - 1;
-        break;
-    }
-
-    d.location.hash = '#' + comments[current].id;
-  }
-
   d.addEventListener('keypress', function(e) {
-    var _c = String.fromCharCode(e.which);
+    var dir, _c = String.fromCharCode(e.which);
 
     // do nothing in these cases
-    if ((_c !== 'c' && _c !== 'C') || (e.target.nodeName == 'TEXTAREA')) return;
+    if ((!(dir = _c == 'c') && _c !== 'C') || (e.target.nodeName == 'TEXTAREA')) return;
 
-    move((_c == 'c') ? 'next' : 'prev');
+    dir ? current++ : current--;
+
+    if ( current > comments.length - 1 ) current = 0;
+    if ( current < 0 ) current = comments.length - 1;
+
+    d.location.hash = '#' + comments[current].id;
   });
-}(document, window))
+}(document));
